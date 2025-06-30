2192df3f48d7f7914c8a241706685a0ca9870c29
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2025-07-01 00:41:33 +0100

    Styling

--- src/services/apiContact.js
+++ src/services/apiContact.js
@@ -1,33 +1,28 @@
-import apiClient from "./api"
+import apiClient from "./api";
 
-const createContact = async (details) =>{
-    const { data } = await apiClient.post('/merchants/contacts', details);
-
-    return data
-}
-
-const getAllContact = async () =>{
-    const { data } = await apiClient.get('/merchants/contacts');
-
+const createContact = async (contactData) => {
+    const { data } = await apiClient.post('/merchants/contacts', contactData);
     return data;
-}
+};
 
-const deleteContact = async (details)=>{
-    const { data } = await apiClient.post('/merchants/contact/delete', details);
-
+const getAllContact = async (page = 1) => {
+    const { data } = await apiClient.get(`/merchants/contacts?page=${page}`);
     return data;
-}
+};
 
-const createGroup = async (groupData) =>{
-    const { data } = await apiClient.post('/merchants/contact-groups', groupData);
-
-    return {data}
-}
-
-const getAllGroups = async ()=>{
-    const { data } = await apiClient.get('/merchants/contact-groups');
-
+const deleteContact = async (contactId) => {
+    const { data } = await apiClient.delete('/merchants/contacts', contactId);
     return data;
-}
+};
 
-export { createContact, getAllContact, deleteContact, createGroup, getAllGroups };
+const createGroup = async (groupData) => {
+    const { data } = await apiClient.post('/merchants/groups', groupData);
+    return data;
+};
+
+const getAllGroups = async () => {
+    const { data } = await apiClient.get('/merchants/groups');
+    return data;
+};
+
+export  {createContact, getAllContact, deleteContact, createGroup, getAllGroups}
